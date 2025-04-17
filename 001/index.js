aa2fb0ebea2e11a2b46162b5e3644972855224d4
haruma1618 <justenlinwwt@gmail.com>
2025-04-17 00:03:17 -0700

    -

--- 001/index.js
+++ 001/index.js
@@ -1,4 +1,5 @@
-const game = {"menu": "game1"}
+const game = {"menu": "game1", "essence": new Decimal(1)}
+let start;
 
 function sidebar_btn_click(id) {
     const body_bg_colors = {"btn1": "#1c0040", "btn2": "#1b4000", "btn3": "#004040"}
@@ -7,3 +8,15 @@ function sidebar_btn_click(id) {
         document.body.style.backgroundColor = body_bg_colors[id];
     }
 }
+
+function update(t) {
+    const elapsed = t - start;
+    start = t;
+    
+    game.essence = game.essence.add(elapsed / 1000)
+    document.getElementById("essence-amt").innerHTML = game.essence.toFixed(1)
+    
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
