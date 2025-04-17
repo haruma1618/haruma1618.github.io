ad06896e5d00116a4769b2696e9f3d9506787562
haruma1618 <justenlinwwt@gmail.com>
2025-04-16 22:43:26 -0700

    -

--- 001/index.js
+++ 001/index.js
@@ -1,6 +1,10 @@
 const body = document.getElementsByTagName("body")[0]
 
-function click(id) {
-    body.style.backgroundColor = "#000000";
-    console.log(body)
+function btn_click(id) {
+    body_bg_colors = {"btn1": "#1c0040", "btn2": "#1b4000", "btn3": "#004040"}
+
+    if (id in body_bg_colors) {
+        body.style.backgroundColor = body_bg_colors[id];
+        console.log(body)
+    }
 }
