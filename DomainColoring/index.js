5da2cff852c7c5db6b93426721623217a1223ac7
haruma1618 <justenlinwwt@gmail.com>
2025-10-14 20:39:26 -0700

    Add more complex functions

--- DomainColoring/index.js
+++ DomainColoring/index.js
@@ -52,51 +52,71 @@ function getFrag(f) {
     /****** Complex Function Definitions ******/
     vec2 cx_add(vec2 a, vec2 b) {return vec2(a.x+b.x, a.y+b.y);}
     vec2 cx_add(vec2 a, float b) {return vec2(a.x+b, a.y);}
-    vec2 cx_add(float a, vec2 b) {return vec2(b.x+a, b.y);}
-    float cx_add(float a, float b) {return a+b;}
+    vec2 cx_add(float a, vec2 b) {return vec2(a+b.x, b.y);}
+    vec2 cx_add(float a, float b) {return vec2(a+b, 0.0);}
 
     vec2 cx_sub(vec2 a, vec2 b) {return vec2(a.x-b.x, a.y-b.y);}
     vec2 cx_sub(vec2 a, float b) {return vec2(a.x-b, a.y);}
-    vec2 cx_sub(float a, vec2 b) {return vec2(b.x-a, b.y);}
-    float cx_sub(float a, float b) {return a-b;}
+    vec2 cx_sub(float a, vec2 b) {return vec2(a-b.x, -b.y);}
+    vec2 cx_sub(float a, float b) {return vec2(a-b, 0.0);}
 
     vec2 cx_mul(vec2 a, vec2 b) {return vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x);}
-    vec2 cx_mul(vec2 a, float b) {return vec2(b*a.x, b*a.y);}
+    vec2 cx_mul(vec2 a, float b) {return vec2(a.x*b, a.y*b);}
     vec2 cx_mul(float a, vec2 b) {return vec2(a*b.x, a*b.y);}
-    float cx_mul(float a, float b) {return a*b;}
+    vec2 cx_mul(float a, float b) {return vec2(a*b, 0.0);}
         
     vec2 cx_div(vec2 a, vec2 b) {return vec2((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y), (a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y));}
     vec2 cx_div(float a, vec2 b) {return vec2(a*b.x/(b.x*b.x+b.y*b.y), -a*b.y/(b.x*b.x+b.y*b.y));}
     vec2 cx_div(vec2 a, float b) {return vec2(a.x/b, a.y/b);}
-    float cx_div(float a, float b) {return a/b;}
+    vec2 cx_div(float a, float b) {return vec2(a/b, 0.0);}
 
+    #define cx_ix(z) vec2(-z.y, z.x)
     #define cx_mod(z) length(z)
     #define cx_arg(z) atan(z.y, z.x)
     #define cx_exp(z) vec2(cos(z.y), sin(z.y))*exp(z.x)
     #define cx_log(z) vec2(log(length(z)), atan(z.y, z.x))
-    #define cx_cos(z) vec2(cos(z.x) * cosh(z.y), -sin(z.x) * sinh(z.y))
-    #define cx_sin(z) vec2(sin(z.x) * cosh(z.y), cos(z.x) * sinh(z.y))
-    #define cx_tan(z) vec2(sin(2.0*z.x), sinh(2.0*z.y)) / (cos(2.0*z.x)+cosh(2.0*z.y))
+    #define cx_ln(z) cx_log(z)
 
-    vec2 cx_pow(vec2 a, vec2 b) {
-        return cx_exp(cx_mul(b, cx_log(a)));
-    }
-    vec2 cx_pow(vec2 a, float b) {
-        return pow(cx_mod(a), b) * vec2(cos(cx_arg(a) * b), sin(cx_arg(a) * b));
-    }
-    vec2 cx_pow(float a, vec2 b) {
-        return cx_exp(log(a) * b);
-    }
-    float cx_pow(float a, float b) {
-        return pow(a, b);
-    }
+    vec2 cx_cos(vec2 z) {return vec2(cos(z.x) * cosh(z.y), -sin(z.x) * sinh(z.y));}
+    vec2 cx_sin(vec2 z) {return vec2(sin(z.x) * cosh(z.y), cos(z.x) * sinh(z.y));}
+    vec2 cx_tan(vec2 z) {return vec2(sin(2.0*z.x), sinh(2.0*z.y)) / (cos(2.0*z.x)+cosh(2.0*z.y));}
+    vec2 cx_sec(vec2 z) {return cx_div(1.0, cx_cos(z));}
+    vec2 cx_csc(vec2 z) {return cx_div(1.0, cx_sin(z));}
+    vec2 cx_cot(vec2 z) {return cx_div(1.0, cx_tan(z));}
+
+    vec2 cx_pow(vec2 a, vec2 b) {return cx_exp(cx_mul(b, cx_log(a)));}
+    vec2 cx_pow(vec2 a, float b) {return pow(cx_mod(a), b) * vec2(cos(cx_arg(a) * b), sin(cx_arg(a) * b));}
+    vec2 cx_pow(float a, vec2 b) {return cx_exp(log(a) * b);}
+    vec2 cx_pow(float a, float b) {return vec2(pow(a, b), 0.0);}
+
+    vec2 cx_sqrt(vec2 z) {return cx_pow(z, 0.5);}
+    vec2 cx_arcsin(vec2 z) {return -cx_ix(cx_log(cx_add(cx_sqrt(cx_sub(1.0, cx_mul(z, z))), cx_ix(z))));}
+    vec2 cx_arccos(vec2 z) {return cx_sub(pi, 2.0 * cx_arcsin(z)) / 2.0;}
+    vec2 cx_arctan(vec2 z) {return -cx_ix(cx_log(cx_div(cx_add(1.0, cx_ix(z)), cx_sub(1.0, cx_ix(z))))) / 2.0;}
+    vec2 cx_arccsc(vec2 z) {return cx_arcsin(cx_div(1.0, z));}
+    vec2 cx_arcsec(vec2 z) {return cx_arccos(cx_div(1.0, z));}
+    vec2 cx_arccot(vec2 z) {return cx_arctan(cx_div(1.0, z));}
+    
+    vec2 cx_cosh(vec2 z) {return cx_cos(cx_ix(z));}
+    vec2 cx_sinh(vec2 z) {return -cx_ix(cx_sin(cx_ix(z)));}
+    vec2 cx_tanh(vec2 z) {return -cx_ix(cx_tan(cx_ix(z)));}
+    vec2 cx_sech(vec2 z) {return cx_div(1.0, cx_cosh(z));}
+    vec2 cx_csch(vec2 z) {return cx_div(1.0, cx_sinh(z));}
+    vec2 cx_coth(vec2 z) {return cx_div(1.0, cx_tanh(z));}
+
+    vec2 cx_arsinh(vec2 z) {return cx_log(cx_add(z, cx_sqrt(cx_add(cx_mul(z, z), 1.0))));}
+    vec2 cx_arcosh(vec2 z) {return cx_log(cx_add(z, cx_sqrt(cx_sub(cx_mul(z, z), 1.0))));}
+    vec2 cx_artanh(vec2 z) {return cx_log(cx_div(cx_add(1.0, z), cx_sub(1.0, z))) / 2.0;}
+    vec2 cx_arcsch(vec2 z) {return cx_arsinh(cx_div(1.0, z));}
+    vec2 cx_arsech(vec2 z) {return cx_arcosh(cx_div(1.0, z));}
+    vec2 cx_arcoth(vec2 z) {return cx_artanh(cx_div(1.0, z));}
 
     vec3 hsl2rgb(in vec3 c) {
         vec3 rgb = clamp(abs(mod(c.x * 6.0 + vec3(0.0, 4.0, 2.0), 6.0) - 3.0) - 1.0, 0.0, 1.0);
         return c.z + c.y * (rgb - 0.5) * (1.0 - abs(2.0 * c.z - 1.0));
     }
 
-    #define f(z) vec2(` + f + `)
+    #define f(z) ` + f + `
     
     void main() {
         vec2 z = vTexCoord.xy * graphSize - graphSize/2.0 + graphCenter;
@@ -189,6 +209,13 @@ function changeFunc(f) {
         }   
     }
 
+    // Check if function is constant
+    if (/^-?\d+(\.\d+)?$/.test(nf)) {
+        nf = "vec2(" + nf + ",0.0)";
+        dcShader = createShader(getVert(), getFrag(nf));
+        return;
+    }
+
     // Replace unary subtraction operators, since they break things
     let ind = 0;
     while (ind < nf.length) {
@@ -214,10 +241,12 @@ function changeFunc(f) {
     nf = replaceWithFuncNotation(nf, "^", "pow");
 
     // From longest to shortest (important)
-    const cx_funcs = ["add", "sub", "mul", "div", "mod", "arg", "exp", "log", "cos", "sin", "tan", "pow"];
+    const cx_funcs = ["arcsin", "arccos", "arctan", "arccsc", "arcsec", "arccot", "arsinh", "arcosh", "artanh", "arcsch", "arsech", "arcoth",
+        "sqrt", "sinh", "cosh", "tanh", "sech", "csch", "coth",
+        "add", "sub", "mul", "div", "mod", "arg", "exp", "log", "cos", "sin", "tan", "pow", "sec", "csc", "cot", "ln"];
 
     for (let s of cx_funcs) {
-        nf = nf.replaceAll(s, "cx_" + s);
+        nf = nf.replaceAll(new RegExp("(^|[(,])(" + s + ")", "g"), "$1cx_$2");
     }
 
     console.log(nf);
@@ -250,6 +279,8 @@ function setup() {
 
         changeFunc(func);
     });
+
+    funcInput.value = "tan(z)";
 }
 
 function draw() {  
@@ -267,17 +298,6 @@ function draw() {
     }
 }
 
-function keyPressed(e) {
-    if (e.key === " ") {
-        prevFunc = func;
-        func = prompt("Enter a function f(z): ");
-
-        if (func !== null) {
-            changeFunc(func);
-        }
-    }
-}
-
 function mouseWheel(e) {
     let zoomFactor = 1.05;
     if (e.delta > 0) { // zoom out
@@ -292,8 +312,10 @@ function mouseWheel(e) {
 }
 
 function mouseDragged(e) {
-    graphMid[0] -= e.movementX * pixelSize;
-    graphMid[1] += e.movementY * pixelSize;
+    if (document.activeElement != getFuncInput()) {
+        graphMid[0] -= e.movementX * pixelSize;
+        graphMid[1] += e.movementY * pixelSize;
+    }
 }
 
 function windowResized(){
